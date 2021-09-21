6b5c485a599f670ee4dcc7331e4380a9da6017ae
Schandroid243 <schadrackngunza@gmail.com>
2021-09-21 13:40:42 +0100

    Added Tachyons Creating my first react component in the hello.js file

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hello.css';
+
+const Hello = () => {
+    return(
+        <div>
+            <h1>Hello</h1>
+            <h3>This is my first react component !</h3>
+        </div>
+    );
+}
+
+export default Hello;
