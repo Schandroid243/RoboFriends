cf6af6959da21843d3058c5d673715155d0e4577
Schandroid243 <schadrackngunza@gmail.com>
2021-09-21 13:58:18 +0100

    Refactoring the usage of props

--- src/Hello.js
+++ src/Hello.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Hello.css';
 
-const Hello = ({greeting}) => {
+const Hello = (props) => {
     return(
         <div className='f1 tc'>
             <h1>Hello</h1>
             <h3>This is my first react component !</h3>
-            <p>{greeting}</p>
+            <p>{props.greeting}</p>
         </div>
     );
 }
